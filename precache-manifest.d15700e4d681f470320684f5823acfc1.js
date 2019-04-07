fc728837c7fef6483f330078a5456d0f42fb69c6
Silpa <silpashinto@gmail.com>
2019-04-06 20:19:47 -0400

    Updates

---
+++ precache-manifest.d15700e4d681f470320684f5823acfc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clickyGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  },
+  {
+    "revision": "37bb726b4312102e40cb",
+    "url": "/clickyGame/static/js/main.8c178dd8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clickyGame/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "37bb726b4312102e40cb",
+    "url": "/clickyGame/static/css/main.c18da295.chunk.css"
+  },
+  {
+    "revision": "cd1cf2fb6e1ba519b900d34f86516973",
+    "url": "/clickyGame/index.html"
+  }
+];
